08b80f84cd6aa964c160ec609f73b081bda07e9d
Aditya Dhanraj <dhanrajadity201@gmail.com>
2024-11-11 19:26:35 +0530

    starting

---
+++ PBL/server/Server.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const cors=require('cors');
+const app=express();
+const port=4000;
+
+app.use(cors({
+    origin:"*",
+}));
+
+app.get('/test',(request,response)=>{
+    const data={  message:"Hello,from server!"  }
+    response.json(data);
+});
+
+
+app.listen(port,function(){
+    console.log(`Server Listening on http://localhost:${port}`);
+});
